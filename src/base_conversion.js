2bf708b99de6524f9a7ba3f3cd1d3b5397abce58
Carlos Rodrigues <carlos.all.mail@gmail.com>
2018-02-11 00:59:20 -0700

    Added comments and some explicitness

--- src/base_conversion.js
+++ src/base_conversion.js
@@ -1,6 +1,10 @@
 function base_converter(value, source, target) {
+  // First convert the value into decimal,
+  // Then convert the decimal into the target alphabet
   return to_any(to_decimal(value, source), target)
 
+  // Converts the value string to a decimal number and returns that number.
+  // NOTE: the length of the source alphabet is the base power of that alphabet
   function to_decimal(value, source) {
     const base = source.length
     return value.split("").reduce(
@@ -9,14 +13,18 @@ function base_converter(value, source, target) {
     )
   }
 
+  // Given a decimal sum, converts that into another number of any alphabet
   function to_any(sum, target) {
     const base = target.length
+    if (sum < base)
+      return target[sum]
+
     let answer = ""
     for (let digit = 0; sum > 0; sum = (sum - digit)/base) {
       digit = sum % base
       answer = target[digit] + answer
     }
-    return answer ? answer : target[sum]
+    return answer
   }
 }
 
