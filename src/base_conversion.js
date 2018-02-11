2c4bda1d43fda58900a622cc4d0e3539d18d6142
Carlos Rodrigues <carlos.all.mail@gmail.com>
2018-02-11 00:52:28 -0700

    Solved exercise.  Solution uses nested functions for clarity

--- src/base_conversion.js
+++ src/base_conversion.js
@@ -1,4 +1,23 @@
 function base_converter(value, source, target) {
+  return to_any(to_decimal(value, source), target)
+
+  function to_decimal(value, source) {
+    const base = source.length
+    return value.split("").reduce(
+      (acc, ele) => acc * base + source.indexOf(ele),
+      0
+    )
+  }
+
+  function to_any(sum, target) {
+    const base = target.length
+    let answer = ""
+    for (let digit = 0; sum > 0; sum = (sum - digit)/base) {
+      digit = sum % base
+      answer = target[digit] + answer
+    }
+    return answer ? answer : target[sum]
+  }
 }
 
 module.exports = base_converter
